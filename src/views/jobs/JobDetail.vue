c72d79012df0d3a979ba10ad7e1b7055fcebcd12
arridhaamrad <arridhaamrad@gmail.com>
2021-02-24 16:59:14 +0700

    fetch data

--- src/views/jobs/JobDetail.vue
+++ src/views/jobs/JobDetail.vue
@@ -1,11 +1,31 @@
 <template>
-  <h1>Job detail view</h1>
-  <p>view job with id : {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>{{ job.description }}</p>
+  </div>
+  <div v-else>
+    <p>loading...</p>
+  </div>
 </template>
 
 <script>
+import Axios from "axios";
 export default {
   props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    Axios.get(`http://localhost:3000/jobs/${this.id}`)
+      .then((res) => (this.job = res.data))
+      .catch((err) => console.log(err.message));
+    // fetch("http://localhost:3000/jobs/" + this.id)
+    //   .then((res) => res.json())
+    //   .then((data) => (this.job = data))
+    //   .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Available Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
-    <router-link :to="{ name: 'JobDetail', params: { id: job.id } }">
-      <h2 class="job">{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id">
+      <router-link :to="{ name: 'JobDetail', params: { id: job.id } }">
+        <h2 class="job">{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>loading...</p>
   </div>
 </template>
 
@@ -11,33 +16,14 @@
 export default {
   data() {
     return {
-      jobs: [
-        {
-          id: 1,
-          title: "Front-End Developer",
-          description:
-            "Nulla turpis purus, posuere at dignissim mollis, viverra a diam. Cras et luctus dui. Nunc nec condimentum turpis, non sollicitudin diam. Sed tincidunt elit nec egestas tempor. Integer sit amet arcu lorem.",
-        },
-        {
-          id: 2,
-          title: "Back-End Developer",
-          description:
-            "Donec at mauris vel enim hendrerit sagittis. Maecenas fringilla porta sapien efficitur feugiat. Phasellus mollis sed tellus sit amet consequat. Pellentesque euismod urna auctor faucibus finibus. In bibendum, leo et convallis maximus, lacus ante auctor felis, ut fringilla mauris tortor in odio.",
-        },
-        {
-          id: 3,
-          title: "Android Developer",
-          description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tellus libero, venenatis ac fermentum at, pulvinar vitae libero. Ut vitae nulla sit amet magna auctor pulvinar.",
-        },
-        {
-          id: 4,
-          title: "IOS Developer",
-          description:
-            "Pellentesque auctor sem augue. Nulla facilisi. Nullam gravida ipsum ut lacus ultricies, eu maximus nulla hendrerit. Nam vel lorem in odio interdum vestibulum eu sed felis. Suspendisse aliquam eros quis libero gravida, ut scelerisque purus mattis.",
-        },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
